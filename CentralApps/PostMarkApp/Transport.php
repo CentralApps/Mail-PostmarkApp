2b7382f02bcf63771b96df73ca0ef10fb85bbb7d
Michael Peacock <mkpeacock@gmail.com>
2012-09-23 22:15:10 +0100

    Permitted attachment types

--- CentralApps/PostMarkApp/Transport.php
+++ CentralApps/PostMarkApp/Transport.php
@@ -5,7 +5,14 @@ class Transport implements \CentralApps\Mail\Transport {
 	
 	protected $apiKey;
 	protected $apiEndPoint = 'http://api.postmarkapp.com/email';
-	protected $permittedAttachmentTypes = array();
+
+	/**
+	 * Allowed attachment file types see: http://developer.postmarkapp.com/developer-build.html#attachments
+	 * - and where appropriate, their corresponding mimetypes
+	 * @var array
+	 */
+	protected $permittedAttachmentTypes = array( 'gif' => 'image/gif', 'jpg' => 'image/jpeg', 'jpeg' => 'image/jpeg', 'png' => 'image/png', 'swf' => 'application/x-shockwave-flash', 'flv' => 'video/x-flv', 'avi' => 'video/x-msvideo', 'mpg' => 'video/mpeg', 'mp3' => 'audio/mpeg', 'rm' => 'application/vnd.rn-realmedia', 'mov' => 'video/quicktime', 'psd' => 'image/psd', 'ai' => 'application/postscript', 'tif' => 'image/tiff', 'tiff' => 'image/tiff', 'txt' => 'text/plain', 'rtf' => 'text/richtext', 'htm' => 'text/html', 'html' => 'text/html', 'pdf' => 'application/pdf', 'doc' => 'application/msword', 'docx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'ppt' => 'application/vnd.ms-powerpoint', 'pptx' => 'application/vnd.openxmlformats-officedocument.presentationml.presentation', 'xls' => 'application/vnd.ms-excel', 'xlsx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'ps' => 'image/psd', 'eps' => 'application/postscript', 'log' => 'text/plain', 'csv' => 'text/csv', 'ics' => 'text/calendar', 'xml' => 'text/xml');
+	
 	protected $maxAttachmentSize;
 	protected $maxNumAttachments = 0;
 	
@@ -80,6 +87,9 @@ class Transport implements \CentralApps\Mail\Transport {
 	
 	protected function attachmentCheckFileTypes($attachment)
 	{
+		if(!array_key_exists($attachment->getExtension(), $this->permittedAttachmentTypes)) {
+			return false;
+		}
 		return true;
 	}
 	
