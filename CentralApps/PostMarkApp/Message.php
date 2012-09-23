42b762b7c7f021c03120111f97fb753f7352e652
Michael Peacock <mkpeacock@gmail.com>
2012-09-23 22:15:17 +0100

    Support for attachments

--- CentralApps/PostMarkApp/Message.php
+++ CentralApps/PostMarkApp/Message.php
@@ -5,6 +5,10 @@ class Message extends \CentralApps\Mail\Message {
 	
 	protected $tag = null;
 	
+	// TODO: try and removed this duplication also in Transport.php
+	protected $permittedAttachmentTypes = array( 'gif' => 'image/gif', 'jpg' => 'image/jpeg', 'jpeg' => 'image/jpeg', 'png' => 'image/png', 'swf' => 'application/x-shockwave-flash', 'flv' => 'video/x-flv', 'avi' => 'video/x-msvideo', 'mpg' => 'video/mpeg', 'mp3' => 'audio/mpeg', 'rm' => 'application/vnd.rn-realmedia', 'mov' => 'video/quicktime', 'psd' => 'image/psd', 'ai' => 'application/postscript', 'tif' => 'image/tiff', 'tiff' => 'image/tiff', 'txt' => 'text/plain', 'rtf' => 'text/richtext', 'htm' => 'text/html', 'html' => 'text/html', 'pdf' => 'application/pdf', 'doc' => 'application/msword', 'docx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'ppt' => 'application/vnd.ms-powerpoint', 'pptx' => 'application/vnd.openxmlformats-officedocument.presentationml.presentation', 'xls' => 'application/vnd.ms-excel', 'xlsx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'ps' => 'image/psd', 'eps' => 'application/postscript', 'log' => 'text/plain', 'csv' => 'text/csv', 'ics' => 'text/calendar', 'xml' => 'text/xml');
+	
+	
 	public function generateSendableArray()
 	{
 		$sendable = array();
@@ -39,6 +43,19 @@ class Message extends \CentralApps\Mail\Message {
 		
 		if(count($this->cc) > 0 ) {
 			$sendable['Cc'] = implode(', ', $this->cc->flattern() );
+		}
+		
+		if(count($this->attachments) > 0 ) {
+			$attachments = array();
+			foreach($this->attachments as $attachment ) {
+				if($attachment->isReadable()) {
+					$attachment['Name'] = $attachment->getFilename();
+					$attachment['Content'] = base64_decode(file_get_contents($attachment->getPath() . '/' . $attachment->getFileName() ) );
+					$attachment['ContentType'] = array_key_exists($attachment->getExtension,$this->permittedAttachmentTypes) ? $this->permittedAttachmentTypes[$attachment->getExtensio()] : 'application/octet-stream';
+					$attachments[] = $attachment;
+				}
+			}
+			$sendable['Attachments'] = $attachments;
 		}
 
 		
